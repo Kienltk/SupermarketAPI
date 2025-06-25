63e63f5f46ec91f5e510e1ae56bd0f4e5f2aee33
Lê Trung Kiên <kienlt.dv@gmail.com>
2025-06-25 14:48:37 +0700

    Fix Update
    Delete  Street, City, State, HomePhone in RegisterDTO

--- DTOs/Request/RegisterDto.cs
+++ DTOs/Request/RegisterDto.cs
@@ -5,11 +5,7 @@
         public string FirstName { get; set; } = null!;
         public string? MiddleName { get; set; }
         public string LastName { get; set; } = null!;
-        public string? Street { get; set; }
-        public string? City { get; set; }
-        public string? State { get; set; }
         public required string Country { get; set; }
-        public string? HomePhone { get; set; }
         public required string Mobile { get; set; }
         public string Email { get; set; } = null!;
         public DateOnly Dob { get; set; }
