bc8e7197b0e19c0c4ce09c18f84d92cfa110ab71
KienLe <kienlt.dv@gmail.com>
2025-07-06 09:19:40 +0700

    Update token-rating
    Add fullname to token and customerId to getRating(product detail and get rating by product slug)

--- DTOs/Response/RatingDto.cs
+++ DTOs/Response/RatingDto.cs
@@ -5,6 +5,7 @@
         public int RatingId { get; set; }
         public int RatingScore { get; set; }
         public string? Comment { get; set; }
+        public int CustomerId { get; set; }
         public string CustomerName { get; set; }
         public DateTime CreatedAt { get; set; }
     }
