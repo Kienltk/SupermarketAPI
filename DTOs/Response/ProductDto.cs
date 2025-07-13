cb286bb0d5733842dd8ee781e1a59c852f01f253
Chienne <154740465+Chiennne2803@users.noreply.github.com>
2025-07-13 21:36:41 +0700

    Update ProductDto.cs

--- DTOs/Response/ProductDto.cs
+++ DTOs/Response/ProductDto.cs
@@ -3,6 +3,7 @@
     public class ProductDto
     {
         public int ProductId { get; set; }
+        public int? CategoryId { get; set; }
 
         public string ProductName { get; set; } = null!;
 
