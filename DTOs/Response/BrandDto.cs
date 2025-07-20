f19b1bc1c8e4ab22f299b9c533573847d25d5082
Chienne <154740465+Chiennne2803@users.noreply.github.com>
2025-07-20 20:00:05 +0700

    Update BrandDto.cs

--- DTOs/Response/BrandDto.cs
+++ DTOs/Response/BrandDto.cs
@@ -1,9 +1,14 @@
-﻿namespace SupermarketAPI.DTOs.Response
+using System.ComponentModel.DataAnnotations;
+
+namespace SupermarketAPI.DTOs.Response
 {
     public class BrandDto
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Brand name is required.")]
         public string BrandName { get; set; }
+        [Required(ErrorMessage = "Slug is required.")]
+        [RegularExpression(@"^[a-z0-9]+(?:-[a-z0-9]+)*$", ErrorMessage = "Slug must be lowercase, hyphen-separated")]
         public string Slug { get; set; }
     }
 }
