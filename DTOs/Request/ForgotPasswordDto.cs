3b13f15eeb0fd6d11002c2416415045d53a78745
Chienne <154740465+Chiennne2803@users.noreply.github.com>
2025-07-20 19:56:08 +0700

    Update ForgotPasswordDto.cs

--- DTOs/Request/ForgotPasswordDto.cs
+++ DTOs/Request/ForgotPasswordDto.cs
@@ -2,6 +2,8 @@
 {
     public class ForgotPasswordDto
     {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
         public string Email { get; set; }
     }
 }
