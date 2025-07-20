6527c448bbb1a1fac2f1b63323391ba8d703f9fa
Chienne <154740465+Chiennne2803@users.noreply.github.com>
2025-07-20 19:58:00 +0700

    Update ResetPasswordDto.cs

--- DTOs/Request/ResetPasswordDto.cs
+++ DTOs/Request/ResetPasswordDto.cs
@@ -1,8 +1,13 @@
-﻿namespace SupermarketAPI.DTOs.Request
+using System.ComponentModel.DataAnnotations;
+
+namespace SupermarketAPI.DTOs.Request
 {
     public class ResetPasswordDto
     {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "New password is required.")]
         public string NewPassword { get; set; }
     }
 }
